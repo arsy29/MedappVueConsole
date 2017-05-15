3d38d34adf127fc9a83486f7f1c48af7f4008185
arsy <arseniosarmiento@gmail.com>
2017-05-16 00:32:23 +0800

    changed login screen using el-card

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,25 +1,36 @@
 <template>
-    <el-dialog title="Log in to What's App Doc Console" :visible.sync="displayLoginPopUp" size="tiny" top="15%" :close-on-click-modal="false"
-        :close-on-press-escape="false" :show-close="false">
-        <div>
-            <el-form ref="form" :model="loginForm">
-                <el-row class="logo-holder">
-                    <img class="logo" src="../../assets/logo/logo.png" align="middle"/>
-                </el-row>
-                <el-form-item>
-                    <el-input placeholder="Username" v-model="loginForm.username">
-                    </el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-input type="password" placeholder="Password" v-model="loginForm.password">
-                    </el-input>
-                </el-form-item>
-                <el-form-item class="align-right">
-                    <el-button class="submit-button" type="primary" @click="onSubmit">Login</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
-    </el-dialog>
+    <div class="login">
+        <el-row>
+            <el-col :xs="0" :sm="0" :md="4" :lg="7">
+                <br/>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="16" :lg="10">
+                <el-card class="box-card">
+                    <div slot="header">
+                        <h2>Log in to What's App Doc Console</h2>
+                    </div>
+                    <div>
+                        <el-form ref="form" :model="loginForm">
+                            <el-row class="logo-holder">
+                                <img class="logo" src="../../assets/logo/logo.png" align="middle"/>
+                            </el-row>
+                            <el-form-item label="Username:">
+                                <el-input placeholder="Username" v-model="loginForm.username">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item label="Password:">
+                                <el-input type="password" placeholder="Password" v-model="loginForm.password">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item class="align-right">
+                                <el-button class="submit-button" type="primary" @click="onSubmit">Login</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
